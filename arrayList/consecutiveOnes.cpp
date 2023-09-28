72be638b18c601e6cea84b7ad2aa672dfdd094a5
Qmi <minh.pham1126@gmail.com>
2023-09-28 10:50:21 +0700

    consecutiveOnes: ran successful

--- arrayList/consecutiveOnes.cpp
+++ arrayList/consecutiveOnes.cpp
@@ -1,10 +1,29 @@
 #include "../library.h"
-void function(args)
+bool consecutiveOnes(vector<int> &nums)
 {
+    // STUDENT ANSWER
+    if (nums.size() == 0)
+        return true;
+    int countOnes = 0;
+    int countCOnes = 0;
+    int maxCOnes = 0;
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        if (nums[i] == 1)
+        {
+            countOnes++;
+            countCOnes++;
+        }
+        else
+        {
+            maxCOnes = max(maxCOnes, countCOnes);
+            countCOnes = 0;
+        }
+    }
+    return countOnes == maxCOnes;
 }
 int main()
 {
-    int a;
-    cin >> a;
-    return 0;
+    vector<int> nums{0, 1, 1, 1, 9, 8};
+    cout << consecutiveOnes(nums);
 }
